d5ae0b089596c490834db3115e59600e7770b9b8
Erhunse <Zcemeog@ucl.ac.uk>
2024-10-13 20:54:50 +0100

    In this version, Q1 of the tutorial is answered.

--- src/tutorialpkg/mypkg1/mymodule1.py
+++ src/tutorialpkg/mypkg1/mymodule1.py
@@ -3,6 +3,15 @@ mock_database = {
     42: {'name': 'Bob', 'email': 'bob@example.com', 'age': 45},
 }
 
+from pathlib import Path
+import sys
+
+Mypkg2Folder = Path(__file__).parent.parent / 'mypkg2'
+sys.path.append(str(Mypkg2Folder))
+
+from mymodule2_1 import calculate_area_of_circle
+from mymodule2_2 import fetch_user_data
+
 if __name__ == '__main__':
     # The functions are in the modules in mypkg2. You will need to import them.
 
@@ -14,3 +23,10 @@ if __name__ == '__main__':
     print(fetch_user_data(42, mock_database))
 
     # Locate the data file `paralmpics_raw.csv` relative to this file using pathlib.Path. Prove it exists.
+    DataFolder = Path().parent.parent / 'data'
+    paralmpics_raw = DataFolder.joinpath('paralmpics_raw.csv')
+
+    if paralmpics_raw.exists() == True:
+        print("Im blooming")
+    else:
+        print("File not found!")
